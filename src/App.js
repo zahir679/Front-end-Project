dcfac65d2da1798ff4937f5fd20b1e71957fc2cd
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-26 18:01:15 +0000

    feat: add state and modal button to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [modalShown, setModalShown] = useState(null)
+
+  useEffect(() => {
+    return () => {
+      setModalShown(false)
+    }
+  }, [])
+
+  const showModal = () => {
+    setModalShown(true)
+  }
+
+  const hideModal = () => {
+    setModalShown(false)
+  }
+
   return (
     <div className="App">
+      <p>Modal button</p>
+      <button onClick={showModal}>Show</button>
     </div>
   );
 }
