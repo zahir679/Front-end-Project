14576b7dd59cc21611859c084cbc6081d4051a8a
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-29 15:14:24 +0000

    add NavBar component and css to App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import HomeContainer from './containers/HomeContainer';
 
 import { useState, useEffect } from 'react';
+import NavBar from './components/NavBar';
+
 
 function App() {
 
   return (
     <div className="App"> 
+      <NavBar />
       <HomeContainer />
     </div>
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './components.css'
+
+function NavBar() {
+    return (
+        <div className="navBar">
+            <p>Average-Connoisseurs</p>
+            <a >Home</a>
+            <a >Log In</a>
+            <a >Wish List</a>
+            <a >Contact Us</a>
+        </div>
+    )
+}
+
+export default NavBar
