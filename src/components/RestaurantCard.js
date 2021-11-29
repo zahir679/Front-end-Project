c6504dfc94927923568d4dc0f32f620e2f8b1ab1
zahir679 <zahirahmed0512@gmail.com>
2021-11-29 10:35:21 +0000

    created restaurant card

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-function RestaurantCard() {
-    return (
-        <div className = "card">
-            
+function RestaurantCard({restaurant}) {
+    return(
+        <div className={restaurantCard}>
+            <h3>Name:</h3>
+            <p>{restaurant.restaurantName}</p>
+            <h4>Address</h4>
+            <p>{restaurant.address}</p>
+            <h4>Cuisine</h4>
+            <p>{restaurant.cuisine}</p>
+            <h4>Price</h4>
+            <p>{restaurant.price}</p>
+            <h4>Vegetarian?</h4>
+            <p>{restaurant.vegetarian}</p>
+            <h4>Halal?</h4>
+            <p>{restaurant.halal}</p>
+            <h4>Glutenfree?</h4>
+            <p>{restaurant.glutenfree}</p>
+            <h4>averageRating</h4>
+            <p>{restaurant.averageRating}</p>
+            <hr/>
+            <button onClick={() => onQuickView(restaurant.id)}>Show in quick view</button>
         </div>
     )
 }
