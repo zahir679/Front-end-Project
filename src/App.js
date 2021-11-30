9560be02e9f3e4a83c6bdeb3084dac6eef4198d8
tamaraherrington <tamaraherrington24@gmail.com>
2021-11-30 10:35:20 +0000

    create Log In and Form components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import HomeContainer from './containers/HomeContainer';
 import NavBar from './components/NavBar';
+import LogIn from './components/LogIn.js'
 
 function App() {
 
   return (
     <div className="App"> 
       <NavBar />
+      <LogIn/>
       <HomeContainer />
     </div>
 
---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+
+const Form = () =>{
+    return(
+        <div>
+            <form>
+                <label input type = "text"> <h1>Username</h1></label>
+                <label input type = "password"> <h1>Password</h1></label>
+                <button>Next</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
