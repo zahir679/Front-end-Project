b27fa427d7b510a7c948b17ef110ef4252a5413f
Jonathan Hutchinson <jhutchinson531@gmail.com>
2021-11-30 10:47:26 +0000

    add input fields for form and submit element

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,38 @@
 
 const Form = () =>{
+
+    let email;
     return(
         <div>
-            <form>
-                <label input type = "text"> <h1>Username</h1></label>
-                <label input type = "password"> <h1>Password</h1></label>
-                <button>Next</button>
+            <form >
+                <div className="formElement">
+                    <label > <h1>Username</h1></label>
+                    <input type="text" id="email" value={email}  />
+                </div>
+                <div className="formElement">
+                    <label > <h1>Password</h1></label>
+                    <input type="text" id="password" />
+                </div>
+                <div className="formElement">
+                    <input type="submit" value="Next" />
+                </div>
             </form>
         </div>
     )
 }
 
 export default Form;
+
+{/* <form onSubmit={handleFormSubmission}>
+<div className="formElement">
+<label htmlFor="description">Description: </label>
+<input type="text" id="description" value={description} onChange={handleDescriptionChange}/>
+</div>
+<div className="formElement">
+<label htmlFor="priority">Priority: </label>
+<input type="text" id="priority" value={priority} onChange={handlePriorityChange}/>
+</div>
+<div className="formElement">
+<input type="submit" value="Add Task" />
+</div>
+</form> */}
